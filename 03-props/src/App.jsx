2517710a8923d29c4710df8f26dcb92c3991f5bd
GAudrey <gilmantaudrey.ag@gmail.com>
2023-02-17 16:17:46 +0100

    React props - props arguments and map an array

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -10,6 +10,33 @@ const App = () => {
     },
   };
 
+  const users = [
+    {
+      id: 1,
+      name: 'Connor',
+      age: 665,
+      comment: {
+        lastComment: 'Blood is blood.',
+      },
+    },
+    {
+      id: 2,
+      name: 'Ivory',
+      age: 24,
+      comment: {
+        lastComment: 'Seriously, Seth?',
+      },
+    },
+    {
+      id: 3,
+      name: 'Gabrielle',
+      age: 38,
+      comment: {
+        lastComment: 'FFS',
+      },
+    },
+  ];
+
   return (
     <div className="py-10 max-w-7xl mx-auto">
       <header>
@@ -22,6 +49,14 @@ const App = () => {
           <div className="px-4 py-8 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg min-h-96 p-8">
               <User name={user.name} age={user.age} bisou={user.comment.lastComment} />
+              {users.map((u) => (
+                <User
+                  key={u.id}
+                  name={u.name}
+                  age={u.age}
+                  bisou={u.comment.lastComment}
+                />
+              ))}
             </div>
           </div>
         </div>
